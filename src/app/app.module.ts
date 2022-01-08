1d56c8bb56b1492cb22853ccf71bfcfd73be1ee9
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-01-08 20:08:57 +0100

    Initial pages and components added through CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { InfoComponent } from './pages/info/info.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsComponent,
+    InfoComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
