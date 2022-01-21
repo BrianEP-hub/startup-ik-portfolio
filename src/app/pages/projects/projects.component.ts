148831cbc408cc2bded6c11be0ee5c9aecef195b
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-01-21 21:08:46 +0100

    Added content to pages and components

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects = PROJECTS_DATA;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
-  }
+const PROJECTS_DATA: Projects[] = [
+  {
+    name: 'Crypto app',
+    link: 'https://brianep-hub-crypto-tracker.netlify.app/',
+    description: 'Self made crypto tracker',
+    repo: 'https://github.com/BrianEP-hub/cryptoapp',
+  },
+  {
+    name: 'Weather API',
+    link: '',
+    description: 'Web scraper API for weather news',
+    repo: 'https://github.com/BrianEP-hub/github-weather',
+  },
+];
 
+interface Projects {
+  name: string;
+  link: string;
+  description: string;
+  repo: string;
 }
